8e89ac0c3348b74feb2918842f40d390f95c8bb6
Augustin Debureaux <adebureaux@gmail.com>
2022-05-18 12:48:16 +0200

    fix ofstream

--- ex03/srcs/ShrubberyCreationForm.cpp
+++ ex03/srcs/ShrubberyCreationForm.cpp
@@ -28,7 +28,7 @@ void ShrubberyCreationForm::execute(Bureaucrat const & executor) const
 		return;
 	std::string str = this->target;
 	str.append("_shrubbery");
-	std::ofstream file(str);
+	std::ofstream file(str.c_str());
 	for (int i = 0; i < 15; i++)
 	file << "\n\n\n     ccee88oo\n  C8O8O8Q8PoOb o8oo\n dOB69QO8PdUOpugoO9bD\nCgggbU8OU qOp qOdoUOdcb\n    6OuU  /p u gcoUodpP\n      \\\\//  /douUP\n        \\\\////\n         |||/\\\n         |||\\/\n         |||||\n   .....//||||\\....\n\n\n";
 	file.close();
